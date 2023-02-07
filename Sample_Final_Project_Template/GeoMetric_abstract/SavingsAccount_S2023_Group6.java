eb9aae06cb598e4b9024ff99eb343645ff72bcf0
Oscarxu0326 <124076811+Oscarxu0326@users.noreply.github.com>
2023-02-07 16:52:22 -0500

    Update SavingsAccount_S2023_Group6.java

--- Sample_Final_Project_Template/GeoMetric_abstract/SavingsAccount_S2023_Group6.java
+++ Sample_Final_Project_Template/GeoMetric_abstract/SavingsAccount_S2023_Group6.java
@@ -9,17 +9,23 @@ public class SavingsAccount_S2023_Group6 extends BankAccount_S2023_Group6 {
 
   /** Deposit amount to the savings account. Returns the balance after deposit*/
   public double despositSaving(double depositAmount) {
-    return this.accountBalance += despositAmount;
+    this.accountBalance += despositAmount;
+    
+    return this.accountBalance;
   }
 
   /** Withdraw amount from account. Returns the balance after withdrawl */
   public void withdrawSaving(double withdrawAmount) {
-    return this.accountBalance -= withdrawAmount;
+    this.accountBalance -= withdrawAmount;
+    
+    return this.accountBalance;
   }
 
   /** Charge Fees from account. Returns balance after fee */
   public double chargeFees(double fee) {
-    return this.accountBalance -= fee;
+    this.accountBalance -= fee;
+    
+    return this.accountBalance;
   }
 
   /** Print the SavingAccount info */
