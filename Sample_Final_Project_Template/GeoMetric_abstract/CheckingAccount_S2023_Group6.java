235660357f42399e96f2e1c3f1ff35ec9e31d2c6
Joker122402 <dovid.rosoff@gmail.com>
2023-02-07 13:37:35 -0800

    Update CheckingAccount_S2023_Group6.java
    Add deoosit, withdrawl and fee method to checking account

--- Sample_Final_Project_Template/GeoMetric_abstract/CheckingAccount_S2023_Group6.java
+++ Sample_Final_Project_Template/GeoMetric_abstract/CheckingAccount_S2023_Group6.java
@@ -14,6 +14,27 @@ public class CheckingAccount_S2023_Group6 extends BankAccount_S2023_Group6 {
     +UML diagram for application
 */
 
+  // Deposit amount to account. Returns balance after deposit
+  public double despositChecking(double depositAmount){
+    this.accountBalance += depositAmount;
+
+    return this.accountBalance;
+  }
+
+  // Withdraw amount from account. Returns balance after withdrawl
+  public double withdrawChecking(double withdrawlAmount){
+    this.accountBalance -= withdrawlAmount; 
+
+    return this.accountBalance;
+  }
+
+  // Charge Fees for account. Returns balance after fee.
+  public double chargeFees(double fee){
+    this.accountBalance -= fee;
+
+    return this.accountBalance;
+  }
+
   /* Print the CheckingAccount info */
   public void printCheckingAccount_S2023_Group6() {
     System.out.println("Checking Account Information: [" + "Account Number: " + checkingAcctNum + "; " + "Account Balance: " + accountBalance + "]");
