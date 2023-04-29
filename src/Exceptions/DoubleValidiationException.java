c8f0812a95b7a466621b51c53d6cae673d010578
jdermezis <james.dermezis20@stjohns.edu>
2023-04-29 18:00:38 -0400

    added javadoc

--- src/Exceptions/DoubleValidiationException.java
+++ src/Exceptions/DoubleValidiationException.java
@@ -1,7 +1,17 @@
 package Exceptions;
 
+/**
+ * <b> Double Validation Exception </b> <br>
+ * - Exception is thrown when the required user input is not a double <br>
+ * - Extends Exception superclass <br>
+ * - Secure Software Design Decision <br>
+ * 
+ * @author Group 6: James Dermezis, David Rosoff, James Tomasuolo, Oscar Xu
+ *
+ */
+
 public class DoubleValidiationException extends Exception {
-    public DoubleValidiationException(double input){
-        super("Invalid input: "  + input + ". Input must be a double");
+    public DoubleValidiationException(double input) {
+        super("Invalid input: " + input + ". Input must be a double");
     }
 }
